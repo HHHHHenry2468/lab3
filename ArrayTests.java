ac6c92bda59547d62253561ed31f6753b95fbfc7
HHHHHenry2468 <122332202+HHHHHenry2468@users.noreply.github.com>
2023-01-27 16:04:04 -0800

    Added more tests and fixed bugs in the methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.lang.reflect.Array;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -7,6 +10,18 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = {3, 4, 5};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[] {5, 4, 3}, input2);
+
+    int[] input3 = {1, 2, 3, 4, 5};
+    ArrayExamples.reverseInPlace(input3);
+    assertArrayEquals(new int[] {5, 4, 3, 2, 1}, input3);
+
+    int[] input4 = {1, 2, 3, 4};
+    ArrayExamples.reverseInPlace(input4);
+    assertArrayEquals(new int[] {4, 3, 2, 1}, input4);
 	}
 
 
@@ -14,5 +29,14 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+
+    int[] input2 = {3, 4, 5};
+    assertArrayEquals(new int[] {5, 4, 3}, ArrayExamples.reversed(input2));
+
+    int[] input3 = {1, 2};
+    assertArrayEquals(new int[] {2, 1}, ArrayExamples.reversed(input3));
+
+    int[] input4 = {};
+    assertArrayEquals(new int[] {}, ArrayExamples.reversed(input4));
   }
 }
